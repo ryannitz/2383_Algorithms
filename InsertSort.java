2c143e34457b1518136bd7cbfb8baa41b23bb61c
ryannitz <ryan.nitz01@gmail.com>
2020-01-20 14:50:51 -0400

    SelectionSort completed and InsertSort started

---
+++ InsertSort.java
@@ -0,0 +1,25 @@
+public class InsertSort{
+	public static void main(String args[]){
+		int[] A = {7,4,8,2,5,3,9}
+		sort(A);
+		show(A); //display the sorted result A
+	}
+
+	public static void sort(int[] A){
+
+	}
+
+	public static void show(int[] A){
+		String strArray = "Empty array";
+		if(A.length > 0) {
+			strArray = "{" + A[0];
+			for (int i = 1; i < A.length; i++) {
+				strArray += "," + A[i];
+			}
+			strArray += "}";
+		}
+		System.out.println(strArray);
+	}
+
+
+}
