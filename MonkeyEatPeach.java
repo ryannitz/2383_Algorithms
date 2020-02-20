cf7756ce0f14e38265dc0a5e81a2178ff2661146
ryannitz <ryan.nitz01@gmail.com>
2020-02-20 10:14:29 -0400

    completed

--- MonkeyEatPeach.java
+++ MonkeyEatPeach.java
@@ -1,18 +1,18 @@
 public class MonkeyEatPeach {
     public static void main(String args[]){
         int DAYS = 6;
-        int peaches = monkeyEat(190, DAYS);
+        int peaches = monkeyEat(1, DAYS);
         System.out.println("Peaches picked on monday: " + peaches);
 
         int checkPeaches = checkAnswer(peaches, DAYS);
-        System.out.println("Peaches remaining(190) = " + checkPeaches);
+        System.out.println("Peaches remaining(1) = " + checkPeaches);
     }
 
     static int monkeyEat(int peaches, int days){
         if(days == 0){
             return peaches;
         }else{
-            return monkeyEat(peaches*2, days-1);
+            return monkeyEat((peaches*2)+2, days-1);
         }
     }
 
@@ -20,8 +20,7 @@ public class MonkeyEatPeach {
         if(days == 0){
             return peaches;
         }else{
-            System.out.println(peaches);
-            return checkAnswer((peaches/2), days-1);
+            return checkAnswer((peaches-1)/2 , days-1);
         }
     }
 }
